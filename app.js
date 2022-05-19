4f9baa32194924a39b72b806812f386f345b6cba
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-19 20:18:25 +0300

    feat: add partials layout

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+// const express = require("express");
+// const hbs = require("hbs");
+// const app = express();
+  
+// app.set("view engine", "hbs");
+// hbs.registerPartials(__dirname + "/views/partials");
+ 
+// app.use("/contact", function(request, response){
+      
+//     response.render("contact", {
+//         title: "Мои контакты",
+//         email: "gavgav@mycorp.com",
+//         phone: "+1234567890"
+//     });
+// }); 
+ 
+// app.use("/", function(request, response){
+      
+//     response.render("home.hbs");
+// });
+// app.listen(3000);
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+// Для работы с файлами layout установим в проект модуль express-handlebars с помощью следующей команды: npm install express-handlebars
 
-// node app.js Tom 23
+
+
+const express = require("express");
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
+const app = express();
+// Функция expressHbs.engine() осуществляет конфигурацию движка. В частности, свойство layoutsDir задает путь к папке с файлами layout относительно корня каталога проекта. 
+// устанавливаем настройки для файлов layout
+app.engine("hbs", expressHbs.engine(
+    {
+        layoutsDir: "views/layouts", 
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+  
+app.use("/contact", function(_, response){
+       
+    response.render("contact", {
+        title: "Мои контакты",
+        email: "gavgav@mycorp.com",
+        phone: "+1234567890"
+    });
+}); 
+  
+app.use("/", function(_, response){
+       
+    response.render("home.hbs");
+});
+app.listen(3000);
